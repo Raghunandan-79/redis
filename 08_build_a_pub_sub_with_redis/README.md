b344b2a60416df404a33a123badaa52749b90fd7
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-25 20:59:28 +0530

    08_build_a_pub_sub_with_redis

---
+++ 08_build_a_pub_sub_with_redis/README.md
@@ -0,0 +1,15 @@
+# 08_build_a_pub_sub_with_redis
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.3.14. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
---
+++ 08_build_a_pub_sub_with_redis/index.ts
@@ -0,0 +1,28 @@
+// Publisher code
+import express from "express";
+import Redis from "ioredis";
+
+const app = express();
+app.use(express.json());
+
+const publisher = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.post("/notifications", async (req, res) => {
+  const payload = {
+    title: req.body.title || "Default title",
+    createdAt: new Date().toISOString(),
+  };
+
+  const receivers = await publisher.publish(
+    "notifications",
+    JSON.stringify(payload),
+  );
+
+  res.json({
+    message: `Notification sent to ${receivers} subscribers`,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("API is running on port 3000");
+});
